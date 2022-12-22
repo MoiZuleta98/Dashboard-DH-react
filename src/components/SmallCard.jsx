42e4f757f6dfb47ec0e6ec30c753c13e1049dfd5
MoiZuleta98 <moiseszuleta02@hotmail.com>
2022-12-22 01:36:22 -0300

    PreAgrego los props al component smallCard

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-function SmallCard() {
+function SmallCard(props) {
     return (
-        <div class="col-md-4 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Movies in Data Base
+        <div className="col-md-4 mb-4">
+            <div className={`card border-left-${props.color} shadow h-100 py-2`}>
+                <div className="card-body">
+                    <div className="row no-gutters align-items-center">
+                        <div className="col mr-2">
+                            <div className={`text-xs font-weight-bold text-${props.color} text-uppercase mb-1`}>
+                            {props.titulo}
                             </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">21</div>
+                            <div className="h5 mb-0 font-weight-bold text-gray-800">{(props.moneda ? '$'+props.cifra:  props.cifra)}</div>
                         </div>
-                        <div class="col-auto">
-                            <i class="fas fa-film fa-2x text-gray-300"></i>
+                        <div className="col-auto">
+                            <i className={`fas ${props.icono} fa-2x text-gray-300`}></i>
                         </div>
                     </div>
                 </div>
